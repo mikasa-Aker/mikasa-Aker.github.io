12a63b931a2fe9dd346fb5a454742b4ff726baab
mikasa-Aker <arshiya.ghanchi@dcmail.ca>
2025-11-26 10:34:04 -0500

    js

---
+++ Assignment_4/part_3/main.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
