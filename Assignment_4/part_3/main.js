59ad7b328a5db8f5f81d96823e9d500d890867b7
mikasa-Aker <arshiya.ghanchi@dcmail.ca>
2025-11-26 12:26:20 -0500

    solve error

--- Assignment_4/part_3/main.js
+++ Assignment_4/part_3/main.js
@@ -65,14 +65,27 @@ draw() {
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
-    size,
+    size
   );
   balls.push(ball);
 }
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
